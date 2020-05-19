7bbaa4035b7c0da2498a5133c1fe44c6090395f8
Shawn Eary <52432872+shawn-eary@users.noreply.github.com>
2020-05-18 21:12:56 -0500

    Backwards Support Not Quite Right
    Tried to add in support to go backwards.  Forwards support still seems to work in GNU\Linux but wasted a bunch of time this evening trying to troubleshoot some weird bug with a !! prelude index too big crash on MS Windows.  I don't think that's my code I think there maybe something specific to the MS Windows Libraries that was causing that weirdness.  I won't be able to get back to this for a few days because I have personal things I need to take care of besides my day job.

--- eTrexChop.hs
+++ eTrexChop.hs
@@ -1,16 +1,18 @@
-
 -- Please see REFERENCES.md file in same directory as this file
 --
 -- DEPENDENCIES:
 -- 1) cabal update
 -- 2) cabal install xml
+-- 3) cabal install time
 
 import System.Environment
 import System.IO
 import Text.XML.Light
 import Data.Time.Clock
 import Data.Time.Format
--- import Debug.Trace
+import Debug.Trace
+
+
 
 -- https://stackoverflow.com/questions/4174372/haskell-date-parsing-and-formatting
 getTimeOfPoint :: Element -> UTCTime
@@ -21,15 +23,22 @@ getTimeOfPoint e = do
    let theText = onlyText theContent
    let theDate = cdData (theText !! 0)
    let dateString = theDate
-   let timeFromString = parseTimeOrError True defaultTimeLocale "%Y-%m-%dT%H:%M:%SZ" dateString :: UTCTime
+   let timeFromString =
+         parseTimeOrError
+           True
+           defaultTimeLocale
+           "%Y-%m-%dT%H:%M:%SZ"
+           dateString :: UTCTime
    timeFromString
 
 
 -- https://en.wikibooks.org/wiki/Haskell/Pattern_matching
-trimTrkSegInternal :: UTCTime -> [Content] -> bool -> Int -> Int -> [Content]
+trimTrkSegInternal :: UTCTime -> [Content] -> Bool -> Int -> Int -> [Content]
 trimTrkSegInternal baseTime [] goForward offsetInSeconds amountToTrimInSeconds =
    []
-trimTrkSegInternal baseTime (tsc:xs) goForward offsetInSeconds amountToTrimInSeconds = do
+trimTrkSegInternal
+   baseTime (tsc:xs) goForward offsetInSeconds amountToTrimInSeconds = do
+
    let trkSegElems = onlyElems (tsc : [])
    let curTrackPoint = trkSegElems !! 0
    let pointTime = getTimeOfPoint curTrackPoint
@@ -38,35 +47,58 @@ trimTrkSegInternal baseTime (tsc:xs) goForward offsetInSeconds amountToTrimInSec
 
    -- This probably isn't the best idea to chop the number off like that
    -- I'm not going to be picky right now
-   let intSecondsPassed = floor secondsPassed
+   let intSecondsPassed =
+         if goForward then
+             floor secondsPassed
+         else
+             (0 - (floor secondsPassed))
    if intSecondsPassed <= amountToTrimInSeconds then
      trimTrkSegInternal
-       baseTime xs goForward offsetInSeconds amountToTrimInSeconds
-     -- trace ("testA: " ++
-     -- show(intSecondsPassed) ++ " " ++ show(amountToTrimInSeconds))
-     --  (trimTrkSegInternal
-     --    baseTime xs goForward offsetInSeconds amountToTrimInSeconds)
+        baseTime xs goForward offsetInSeconds amountToTrimInSeconds
+     --trace ("testA: " ++
+     --show(intSecondsPassed) ++ " " ++ show(amountToTrimInSeconds))
+     -- (trimTrkSegInternal
+     --   baseTime xs goForward offsetInSeconds amountToTrimInSeconds)
    else
      tsc :
-       trimTrkSegInternal
-       baseTime xs goForward offsetInSeconds amountToTrimInSeconds
-     -- trace "testB"
-     --   (tsc :
-     --     trimTrkSegInternal
-     --     baseTime xs goForward offsetInSeconds amountToTrimInSeconds)
+        trimTrkSegInternal
+        baseTime xs goForward offsetInSeconds amountToTrimInSeconds
+     --trace "testB"
+     --  (tsc :
+     --    trimTrkSegInternal
+     --    baseTime xs goForward offsetInSeconds amountToTrimInSeconds)
 
-trimTrkSeg :: [Content] -> bool -> Int -> Int -> [Content]
--- trimTrkSeg :: [Content] -> bool -> Int -> Int -> IO ()
+
+
+trimTrkSeg :: [Content] -> Bool -> Int -> Int -> [Content]
 trimTrkSeg trkSegContent goForward offsetInSeconds amountToTrimInSeconds = do
-   let trkSegElems = onlyElems trkSegContent
+   let trkSegElems =
+         if goForward then do
+             onlyElems trkSegContent
+         else do
+             reverse (onlyElems trkSegContent)
 
-   let firstTrkPt1 = trkSegElems !! 0
-   let firstPt1Date = getTimeOfPoint firstTrkPt1
-   trimTrkSegInternal firstPt1Date trkSegContent goForward offsetInSeconds amountToTrimInSeconds
+   let startingTrkPt1 = trkSegElems !! 0
+   let startingPt1Date = getTimeOfPoint startingTrkPt1
+
+   let trimmedList =
+         trimTrkSegInternal
+         startingPt1Date
+         trkSegContent
+         goForward
+         offsetInSeconds
+         amountToTrimInSeconds
+   if goForward then
+       trimmedList
+   else
+       trimmedList
+      -- I'm not sure why I'm commenting this out...
+      -- seems I need to reverse the list back...
+      -- reverse trimmedList
 
 
 
-main :: IO() 
+main :: IO()
 main = do
   theArgs <- getArgs
   let argsLength = length theArgs
@@ -84,18 +116,18 @@ main = do
   --  putStrLn "-- and send output to stdout"
   --  putStrLn "./eTrexChop beaverLake.gpx - 0 180"
   --  putStrLn ""
-  if (argsLength /= 2) then do
-    putStrLn "Usage: ./eTrexChop [fileName] [secondsToTrimFromBeginning]"
+  if (argsLength /= 3) then do
+    putStrLn "Usage: ./eTrexChop [fileName] [direction] [secondsToTrim]"
     putStrLn ""
     putStrLn "Examples:"
     putStrLn ""
     putStrLn "-- Trim 10 minutes off the front of the file and "
     putStrLn "-- send output to mmChopped.gpx"
-    putStrLn "./eTrexChop ./mountMagazine.gpx 600 > ./mmChopped.gpx"
+    putStrLn "./eTrexChop ./mountMagazine.gpx + 600 > ./mmChopped.gpx"
     putStrLn ""
-    putStrLn "-- Trim 3 minutes off the front of the file "
+    putStrLn "-- Trim 3 minutes off the end of the file going backwards"
     putStrLn "-- and send output to stdout"
-    putStrLn "./eTrexChop beaverLake.gpx 180"
+    putStrLn "./eTrexChop beaverLake.gpx - 180"
     putStrLn ""
   else do
     --let fileName = theArgs !! 0
@@ -105,7 +137,15 @@ main = do
     let fileName = theArgs !! 0
     let direction = "+"
     let offsetInSecondsStr = "0"
-    let secondsToTrimStr = theArgs !! 1
+    let direction = theArgs !! 1
+
+    let boolDirection =
+         if (direction == "-") then
+           False
+         else
+           True
+
+    let secondsToTrimStr = theArgs !! 2
 
     let offsetInSeconds = read offsetInSecondsStr::Int
     let secondsToTrim = read secondsToTrimStr::Int
@@ -118,18 +158,18 @@ main = do
     -- See [1] for where I got the code
     let contents = parseXML theFile
     --let xmlContent = contents !! 0
-    --print xmlContent 
+    --print xmlContent
     -- let xmlElem = Elem xmlContent
     --let xmlText = Text xmlContent
     --let xmlCRef = CRef xmlContent
-    
+
     --let gpxContent = contents !! 1
     --putStrLn ""
     let theElements = onlyElems contents
 
     let header = theElements !! 0
     putStrLn (showElement header)
-   
+
     let gpx = theElements !! 1
     let gpxContentList = elContent gpx
     -- let gpxElems = onlyElems gpxContentList
@@ -140,7 +180,7 @@ main = do
 
     let gpxName = elName gpx
     let gpxAttribs = (elAttribs gpx)
-    
+
     let gpxContent = (elContent gpx)
 
     let trkContent = gpxContentList !! 1
@@ -151,7 +191,7 @@ main = do
     let trkElemName = elName trkElement
     let trkElemAttribs = elAttribs trkElement
 
-    
+
     let trkContent = (elContent trkElement)
     let nameContent = trkContent !! 0
     let extensionsContent = trkContent !! 1
@@ -165,56 +205,22 @@ main = do
     let trkSegContent = elContent trkSegElement
     let trkSegELLine = elLine trkSegElement
 
-    let trimmedTrackSegContent = trimTrkSeg trkSegContent True 0 secondsToTrim
+    let trimmedTrackSegContent =
+          trimTrkSeg trkSegContent boolDirection 0 secondsToTrim
 
-    let newTrkSegElement = Element trkSegName trkSegAttribs trimmedTrackSegContent trkSegELLine
+    let newTrkSegElement =
+         Element trkSegName trkSegAttribs trimmedTrackSegContent trkSegELLine
     let newTrkSegContent = Elem newTrkSegElement
-    
     let trkElemContent = nameContent : extensionsContent : newTrkSegContent : []
-    
     let trkELLLine = elLine trkElement
-    let newTrkElement = Element trkElemName trkElemAttribs trkElemContent trkELLLine
+    let newTrkElement =
+         Element trkElemName trkElemAttribs trkElemContent trkELLLine
     let trkContent = Elem newTrkElement
-
     let newGPX_content = (gpxContentList !! 0) : trkContent : []
-
-
     let gpxELLine = (elLine gpx)
-
     let newGPX = Element gpxName gpxAttribs newGPX_content gpxELLine
 
-    -- print ""
     putStrLn (showElement newGPX)
 
     -- https://stackoverflow.com/questions/51509949/what-do-haskell-data-constructors-construct
     -- https://stackoverflow.com/questions/10115623/accessing-members-of-a-custom-data-type-in-haskell
---    let attribList = (elAttribs header)
-
-  --  putStrLn "*************"
- --   print attribList
- --   let item1 = attribList !! 0
- --   let item2 = attribList !! 1
---    let item3 = attribList !! 2
-
-  --  let item1Key = attrKey item1
-    
-    --let modifiedAttribList = (Attr item1Key "7.0") : item2 : item3 : []
-   -- putStrLn "*************"
-     
-    --let clonedHeader = Element (elName header) modifiedAttribList (elContent header) (elLine header)
-
-
-
-    -- putStrLn (showElement clonedHeader)
-
-    -- let gpx = theElements !! 1
-    
-    -- putStrLn (showElement gpx)
-
-   --let gpx = theElements !! 1
-  -- print header
-   -- putStrLn ""
-
-
-    
-    
